78315181d9e04ef738f0c505573b4815ef48a96c
seungmin-dev <seungmin.dev@gmail.com>
2022-03-25 16:23:19 +0900

    first commit

---
+++ src/ToDoList.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+function ToDoList() {
+    const [toDo, setToDo] = useState("");
+    const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+        const {
+            currentTarget: {value},
+        } = event;
+        setToDo(toDo);
+    };
+    const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(toDo);
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+                <button>Add</button>
+            </form>
+        </div>
+    )
+}

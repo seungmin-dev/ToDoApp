d5986667c89e77ea142628176f8f9d18054abe7e
seungmin-dev <seungmin.dev@gmail.com>
2022-03-26 14:16:54 +0900

    set error messages for form validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -23,13 +23,66 @@ import { useForm } from "react-hook-form";
 //     )
 // }
 
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  userName: string;
+}
+
 function ToDoList() {
-  const { register, watch } = useForm();
-  
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(errors);
   return (
     <div>
-      <form>
-        <input {...register("toDo")} placeholder="Write a to do" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed.",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("firstName", { required: "firstname is required" })}
+          placeholder="First Name"
+        />
+        <span>{errors?.firstName?.message}</span>
+        <input
+          {...register("lastName", { required: true })}
+          placeholder="Last Name"
+        />
+        <span>{errors?.lastName?.message}</span>
+        <input
+          {...register("userName", {
+            required: "userName is required",
+            minLength: {
+              value: 7,
+              message: "Your username is too short.",
+            },
+          })}
+          placeholder="userName"
+        />
+        <span>{errors?.userName?.message}</span>
         <button>Add</button>
       </form>
     </div>
